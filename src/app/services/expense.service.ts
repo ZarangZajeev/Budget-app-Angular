02380d4f01d32bfc642ad48975d47b583cd3af8d
ZarangZajeev <zarangzajeev@gmail.com>
2024-02-21 13:43:42 +0530

    first commit

---
+++ src/app/services/expense.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExpenseService {
+
+  baseUrl:string="http://127.0.0.1:8000/api/v1/transactions/"
+
+  constructor(private http:HttpClient) { }
+
+  // service for listing all transanctions
+  getTransanction(){
+    return this.http.get(this.baseUrl)
+  }
+
+  // service for retrieving specific transaction
+  retrieveTransanction(id:number){
+    return this.http.get(`${this.baseUrl}${id}/`)
+  }
+
+  // service for create new transaction
+  createTransaction(data:any){
+    return this.http.post(this.baseUrl,data)
+  }
+
+  // service for update new transaction
+  updateTransaction(id:number,data:any){
+    return this.http.put(`${this.baseUrl}${id}`,data)
+  }
+
+  // remove transaction
+  removeTransaction(id:number){
+    return this.http.delete(`${this.baseUrl}${id}`)
+  }
+}
